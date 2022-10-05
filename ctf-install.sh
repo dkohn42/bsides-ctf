5fbeb7c914bf7b5648cf33cb26b4ff1880431f97
dkohn42 <91912860+dkohn42@users.noreply.github.com>
2022-10-05 11:58:52 -0700

    Update ctf-install.sh

--- ctf-install.sh
+++ ctf-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt update -y
+sudo apt upgrade -y
 sudo apt -y install libimage-exiftool-perl steghide john
 sudo mdkir /usr/share/wordlists
 sudo wget -O /usr/share/wordlists/rockyou.txt https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
