edbad7a4bc8e7c6b7eaf83a2eaafc2aefbc0ff17
dkohn42 <91912860+dkohn42@users.noreply.github.com>
2022-10-06 21:44:06 -0700

    Update ctf-install.sh

--- ctf-install.sh
+++ ctf-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt -y install libimage-exiftool-perl steghide john unzip
+sudo apt -y install libimage-exiftool-perl steghide john unzip strings gdb readelf
 sudo mdkir /usr/share/wordlists
 sudo wget -O /usr/share/wordlists/rockyou.txt https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
 wget https://github.com/anaaktge/talks/raw/main/re_station.zip
